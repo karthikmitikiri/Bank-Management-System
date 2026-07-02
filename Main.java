fe3c4e8f202dbd4d7fa828a10b357f992f2fe013
Karthik Mitikiri <karthikmitikiri@gmail.com>
2026-07-02 12:34:52 +0530

    Initialize Banking Management System project

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String bankName = "XYZ BANK", customerName = "Karthik";
+        int accountNumber = 123456789, balance = 5000;
+        System.out.println("***************************");
+        System.out.println("          "+bankName);
+        System.out.println("Welcome to Bank Management System");
+        System.out.println("***************************");
+        System.out.println("Customer Name: " + customerName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+        System.out.println("Thank you for choosing " + bankName);
+
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Banking Management System (BMS)
+
+## Version
+v0.1
+
+## Description
+A Java-based Banking Management System developed as part of my Git & GitHub learning journey.
+
+## Features
+- Deposit Money
+- Withdraw Money
+- Check Balance
+
+## Tech Stack
+- Java
+- Git
+- GitHub
+
+## Author
+Karthik Mitikiri
