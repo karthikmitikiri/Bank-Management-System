b6150453a7c7331e312bdeb51e56041b0d962e36
Karthik Mitikiri <karthikmitikiri@gmail.com>
2026-07-03 19:41:38 +0530

    Deposit feature added

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String bankName = "XYZ BANK", customerName = "Karthik";
-        int accountNumber = 123456789, balance = 5000;
+        int accountNumber = 123456789, balance = 5000, depositAmount = 2000;
         System.out.println("***************************");
         System.out.println("          "+bankName);
         System.out.println("Welcome to Bank Management System");
@@ -9,6 +9,9 @@ public class Main {
         System.out.println("Customer Name: " + customerName);
         System.out.println("Account Number: " + accountNumber);
         System.out.println("Balance: " + balance);
+        System.out.println("Deposit Amount: " + depositAmount);
+        balance += depositAmount;
+        System.out.println("Current Balance: " + balance);
         System.out.println("Thank you for choosing " + bankName);
 
     }
