5b920a36e9d83e855996dddb166f3ac2f4de6719
Karthik Mitikiri <karthikmitikiri@gmail.com>
2026-07-14 22:33:14 +0530

    Move banking operations to BankAccount class

--- AtmSimulator.java
+++ AtmSimulator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class AtmSimulator {
     public static void welcomeMessage(String bankName){
         System.out.println("***************************");
@@ -15,74 +16,49 @@ public class AtmSimulator {
         System.out.println("4. Exit");
     }
 
-    public static void showBalance(long balance){
-        System.out.println("Current Balance: " + balance);
-    }
-
-public static long deposit(long balance, long depositAmount){
-    if(depositAmount <= 0){
-        System.out.println("Invalid deposit amount");
-    }
-    else{
-    balance += depositAmount;
-    System.out.println("Deposit successful.");
-    System.out.println("Current Balance: " + balance);
-    }
-    return balance;
-}
-
-public static long withdraw(long balance, long withdrawAmount){
-    if(withdrawAmount <=0){
-    System.out.println("Invalid withdraw amount");
-    }
-    else if(withdrawAmount <= balance){
-    balance -= withdrawAmount;
-    System.out.println("Withdraw successful.");
-    System.out.println("Current Balance: " + balance);
-    }
-    else{
-    System.out.println("Insufficient balance.");
-    }
-    return balance;
-}
-
     public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String bankName = "XYZ BANK";
         int choice = 0;
-        long  balance = 5000, depositAmount;
+        long depositAmount;
+        BankAccount account = new BankAccount();
+        account.accountHolderName = "Karthik";
+        account.accountNumber = 123456789;
+        account.balance = 5000;
+        account.ifscCode = "SBIN000123";
+
         welcomeMessage(bankName);
         while(choice !=4){
             showMenu();
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
 
-        switch(choice){
-            case 1:
-                showBalance(balance);
-                break;
+            switch(choice){
+                case 1:
+                    account.showBalance();
+                    break;
 
-            case 2:
-                System.out.print("Enter deposit amount: ");
-                depositAmount = sc.nextLong();
-                balance = deposit(balance, depositAmount);
-                break;
+                case 2:
+                    System.out.print("Enter deposit amount: ");
+                    depositAmount = sc.nextLong();
+                    account.deposit(depositAmount);
+                    break;
 
-            case 3:
-                System.out.println("Enter withdraw amount: ");
-                long withdrawAmount = sc.nextLong();
-                balance = withdraw(balance, withdrawAmount);
-                break;
+                case 3:
+                    System.out.print("Enter withdraw amount: ");
+                    long withdrawAmount = sc.nextLong();
+                    account.withdraw(withdrawAmount);
+                    break;
 
-            case 4:
-                System.out.println("Thank you for choosing " + bankName);
-                break;
+                case 4:
+                    System.out.println("Thank you for choosing " + bankName);
+                    break;
 
-            default:
-                System.out.println("Invalid choice.");
-                System.out.println("Please select a number between 1-4.");
+                default:
+                    System.out.println("Invalid choice.");
+                    System.out.println("Please select a number between 1-4.");
+            }
         }
-    }
 
-}
+    }
 }
---
+++ BankAccount.java
@@ -0,0 +1,35 @@
+class BankAccount {
+    long accountNumber;
+    String accountHolderName;
+    long balance;
+    String ifscCode;
+
+    public void showBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public void deposit(long depositAmount){
+        if(depositAmount <= 0){
+            System.out.println("Invalid deposit amount");
+        }
+        else{
+            this.balance += depositAmount;
+            System.out.println("Deposit successful.");
+            System.out.println("Current Balance: " + this.balance);
+        }
+    }
+
+    public void withdraw(long withdrawAmount){
+        if(withdrawAmount <=0){
+            System.out.println("Invalid withdraw amount");
+        }
+        else if(withdrawAmount <= this.balance){
+            this.balance -= withdrawAmount;
+            System.out.println("Withdraw successful.");
+            System.out.println("Current Balance: " + this.balance);
+        }
+        else{
+            System.out.println("Insufficient balance.");
+        }
+    }
+}
